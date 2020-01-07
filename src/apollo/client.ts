54c56ed779c23d3c0039b31cf163cd1d8ca833c8
Kentaro Umetsu <umetsu.kentaro@adways.net>
2020-01-07 11:12:38 +0900

    :sparkles: apollo-boostからapollo-clientへ移行

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,8 +1,29 @@
-import ApolloClient from "apollo-boost"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { ApolloClient } from "apollo-client"
+import { ApolloLink } from "apollo-link"
+import { onError } from "apollo-link-error"
+import { HttpLink } from "apollo-link-http"
 
 export const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  headers: {
-    Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-  },
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.error(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        )
+      }
+      if (networkError) {
+        console.error(`[Network error]: ${networkError}`)
+      }
+    }),
+    new HttpLink({
+      uri: "https://api.github.com/graphql",
+      headers: {
+        Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    }),
+  ]),
+  cache: new InMemoryCache(),
 })
